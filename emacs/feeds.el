cdde230eb4edf2e3043b30df9c5bc4428b51a39f
Arian-D <ArianDehghani@protonmail.com>
2020-12-25 02:12:07 -0800

    Moved config-behavior.el to README.org

---
+++ emacs/feeds.el
@@ -0,0 +1,31 @@
+(defvar my/feeds
+  '(("https://nullprogram.com/feed/" blog emacs general)
+    ("https://news.ycombinator.com/rss" hn general)
+    ("https://drewdevault.com/feed.xml" blog linux sway general)
+    ;; Dev
+    ("https://www.tweag.io/rss.xml" tweag haskell dev)
+    ("https://planetpython.org/rss20.xml" python general)
+    ;; GNU slash Leenouxe
+    ("https://planet.kernel.org/rss20.xml" kernel linux)
+    ("https://guix.gnu.org/feeds/blog.atom" guix linux)
+    ("https://weekly.nixos.org/feeds/all.rss.xml" nixos linux)
+    ("https://trisquel.info/en/node/feed" trisquel linux)
+    ("https://planet.debian.org/atom.xml" debian linux)
+    ("https://planet.ubuntu.com/atom.xml" ubuntu linux)
+    ("https://security.gentoo.org/glsa/feed.rss" gentoo security linux)
+    ("https://planet.gentoo.org/atom.xml" gentoo linux)
+    ("https://fedoraplanet.org/atom.xml" fedora linux)
+    ("https://www.archlinux.org/feeds/news/" arch linux)
+    ;; Security; Im a hax0r b0i, apparently
+    ("https://forum.defcon.org/external?type=rss2&nodeid=19" def-con security)
+    ("https://proofpoint.com/rss.xml" proof-point security)	  
+    ("https://researchcenter.paloaltonetworks.com/unit42/feed" palo-alto security)
+    ("https://reddit.com/r/netsec/.rss" netsec reddit security)
+    ("https://feeds.feedburner.com/feedburner/Talos?format=xml" talos cisco security)
+    ("https://hackaday.com/blog/feed/" hackaday blog security)
+    ("https://feeds.trendmicro.com/Anti-MalwareBlog/" trend-micro security)
+    ;; Quick mafs
+    ("https://golem.ph.utexas.edu/category/" n-category-cafe blog math)
+    ;; Entertainment
+    ("https://xkcd.com/atom.xml" xkcd comics entertainment))
+  "List of my favoriteRSS/Atom feeds")
