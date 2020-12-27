92be7a83c527f474adb5e4c9b353d14d5d53fd1e
Arian-D <ArianDehghani@protonmail.com>
2020-12-27 02:29:54 -0800

    Changed channel to nixos-20.09 in flake.nix and updated flake.lock

--- flake.nix
+++ flake.nix
@@ -1,22 +1,25 @@
 {
   description = "My beautiful flakey Nix config";
   inputs = {
-    nixpkgs.url = "nixpkgs/release-20.09";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-20.09";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = inputs @ { nixpkgs, nixpkgs-unstable, home-manager, ... }: {
     nixosConfigurations.somewhere = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./nixos/configuration.nix
+        nixpkgs.nixosModules.notDetected
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.users.someone = import ./home-manager/home.nix;
+          home-manager.verbose = true;
         }
       ];
     };
