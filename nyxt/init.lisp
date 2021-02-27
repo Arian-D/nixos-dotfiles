0b733703909fa7d963709e1ff4aef45f615482ae
Arian-D <ArianXDehghani@gmail.com>
2021-02-27 01:49:27 -0800

    Nyxt: Fixed the youtube player, and added keybindings

--- nyxt/init.lisp
+++ nyxt/init.lisp
@@ -12,5 +12,18 @@
   "Play a Link in mpv"
   (let ((youtube-uri
 	  (nyxt/web-mode::query-hints "> mpv " 'url)))
-    (play-youtube-video youtube-uri)))
+    (play-video youtube-uri)))
 
+(defvar *my-keymap* (make-keymap "my-map"))
+(define-key *my-keymap*
+  "; ;" 'play-link
+  "; b" 'switch-buffer
+  "C-b" 'nyxt/web-mode:history-backwards)
+
+(define-mode my-mode ()
+  "Dummy mode for the custom key bindings in `*my-keymap*'."
+  ((keymap-scheme (keymap:make-scheme
+		   scheme:vi-normal *my-keymap*))))
+
+(define-configuration (buffer web-buffer)
+  ((default-modes (append '(my-mode) %slot-default))))
