c1392c3d908338d5bcf67da3e34d2d56c4bfa431
Arian-D <ArianXDehghani@gmail.com>
2021-02-26 20:42:00 -0800

    Added nyxt to flakes

--- flake.nix
+++ flake.nix
@@ -7,23 +7,34 @@
       url = "github:nix-community/home-manager/release-20.09";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
+    nyxt = {
+      url = "github:atlas-engineer/nyxt/2-pre-release-5";
+      flake = true;
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
-  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
-    nixosConfigurations.somewhere = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nixos/configuration.nix
-        nixpkgs.nixosModules.notDetected
-      ];
-    };
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, home-manager, nyxt, ... }:
+    {
+      nixosConfigurations.somewhere = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+          nixpkgs.nixosModules.notDetected
+        ];
+      };
 
-    homeConfigurations.home = home-manager.lib.homeManagerConfiguration {
-      configuration = import ./home-manager/home.nix;
-      system = "x86_64-linux";
-      homeDirectory = "/home/someone";
-      username = "someone";
+      homeConfigurations.home =
+        home-manager.lib.homeManagerConfiguration {
+          configuration = { pkgs, ... }:
+            {
+              imports = [./home-manager/home.nix];
+              home.packages = [ nyxt.defaultPackage.x86_64-linux ];
+            };
+          system = "x86_64-linux";
+          homeDirectory = "/home/someone";
+          username = "someone";
+        };
+      home = self.homeConfigurations.home.activationPackage;
     };
-    home = self.homeConfigurations.home.activationPackage;
-  };
 }
