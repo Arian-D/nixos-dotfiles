50f1ea5c05528ea609dafbb889886f736de6adde
Arian-D <ArianXDehghani@gmail.com>
2021-03-23 03:01:34 -0700

    Removed the gaps and added fullscreen

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,6 +9,8 @@ import XMonad.Util.XSelection (promptSelection)
 import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed (shrinkText, tabbed)
 import XMonad.Layout.Accordion -- (Accordion)
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
 import System.IO
 import qualified Data.Map as M
 import System.Environment (lookupEnv)
@@ -19,20 +21,25 @@ main :: IO ()
 main = do
   home <- getHomeDirectory
   setWallpaper $ home </> "wallpaper"
-  xmonad $ docks def
+  xmonad $ fullscreenSupport $  docks def
     { borderWidth = 2
     , modMask = mod4Mask
     , terminal = "alacritty"
     , focusedBorderColor = "#0000ff"
-    , layoutHook =
-      let gap = 10
-          border = Border gap gap gap gap
-      in
-        avoidStruts $ (spacingRaw False-- Smart border
-                       border -- Screen border
-                       True
-                       border -- Window border
-                       True) $ layoutHook def
+    -- , layoutHook =
+    --   let gap = 10
+    --       border = Border gap gap gap gap
+    --   in
+    --     avoidStruts $ (spacingRaw False-- Smart border
+    --                    border -- Screen border
+    --                    True
+    --                    border -- Window border
+    --                    True) $ layoutHook def
+    , layoutHook = avoidStruts (
+        Full |||
+        Tall 1 (3/100) (1/2)) |||
+        noBorders (fullscreenFull Full)
+                   
     , manageHook = manageDocks <+> manageHook def
     , keys = myKeys <+> keys def
     }
